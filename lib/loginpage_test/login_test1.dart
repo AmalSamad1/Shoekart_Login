0f350406b6a60a3026f0f845af676564e47ac591
AmalSamad1 <95689207+AmalSamad1@users.noreply.github.com>
2023-03-14 17:13:58 +0530

    Add files via upload

---
+++ lib/loginpage_test/login_test1.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations/controlled_animation.dart';
+import 'package:simple_animations/simple_animations/multi_track_tween.dart';
+
+class Log1 extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emaillog = new TextEditingController();
+  TextEditingController passlog = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Color.fromARGB(223, 82, 54, 43),
+                  Color.fromARGB(223, 82, 54, 43),
+                  Color.fromARGB(223, 82, 54, 43),
+                ]
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1, Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),)),
+                  SizedBox(height: 10,),
+                  FadeAnimation(1.3, Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                        FadeAnimation(1.4, Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [BoxShadow(
+                                  color: Colors.grey,
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                             SingleChildScrollView(child: Form(
+                               key: _formKey,
+                               child:Column(children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade200))
+                                ),
+                                child: TextFormField(
+
+                                  controller: emaillog,
+                                  validator: (value) {
+                                    if(value!.isEmpty){
+                                      return "Please Enter Email";
+                                    }
+                                    if(!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)){
+                                      return "Please a Valid Email";
+                                    }
+                                    return null;
+
+                                  },
+                                  onSaved:(newValue) {
+                                    emaillog=newValue as TextEditingController;
+                                  } ,
+                                  decoration: InputDecoration(
+                                      hintText: "Email or Phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade200))
+                                ),
+                                child: TextFormField(
+                                  validator: (value){
+                                    if(value!.isEmpty)
+                                    {
+                                      return 'Please a Enter Password';
+                                    }
+                                    return null;
+                                  },
+
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ))]))),
+                        SizedBox(height: 40,),
+                        FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
+                        SizedBox(height: 40,),
+                        FadeAnimation(1.6,
+                           GestureDetector(onLongPress: (){
+    if(_formKey.currentState!.validate())
+    {
+    print("successful");
+    return;
+    }else{
+    print("UnSuccessfull");
+    }
+
+                           },
+                               child: Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Color.fromARGB(223, 82, 54, 43),
+                          ),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                        ))),
+                        SizedBox(height: 50,),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeAnimation(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("opacity").add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track("translateY").add(
+          Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (500 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation["opacity"],
+        child: Transform.translate(
+            offset: Offset(0, animation["translateY"]),
+            child: child
+        ),
+      ),
+    );
+  }
+}
